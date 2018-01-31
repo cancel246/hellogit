306c9acf4c92d6d659d9d6ec371ea992174e1a87
cancel246 <cancel246@users.noreply.github.com>
2018-01-31 11:49:08 -0400

    Added header file

---
+++ grades.h
@@ -0,0 +1,39 @@
+class Grades{
+	public:
+		explicit Grades(string name){
+		:name{g_name};
+		}
+		
+		explicit Grades(float gpa){
+		:gpa{g_gpa};
+		}
+
+		void setName(string name){
+			name = g_name;
+		}
+		
+		string getName(){
+			return name;
+		}
+
+		void setGpa(float gpa){
+			gpa = g_gpa;
+		}
+		
+		float getGpa(){
+			return gpa;
+		}
+
+
+
+
+
+
+
+
+
+
+
+		
+		
+		
